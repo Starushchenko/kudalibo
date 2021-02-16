e0bec2aca2b7a95f866ac6b89d175ff07661abfa
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2021-02-16 17:51:33 +0300

    Сверстано и стилизовано полноэкранное меню https://prnt.sc/zqnpmx

---
+++ components/SiteMenu.vue
@@ -0,0 +1,174 @@
+<template>
+	<div class="site-menu">
+		<h2 class="site-menu__title">Меню сайта</h2>
+		<div class="site-menu__wrap">
+			<nav class="site-menu__block">
+				<span class="site-menu__block-title">Информация</span>
+				<ul class="site-menu__block-list">
+					<li class="site-menu__item">
+						<a href="#" class="site-menu__item-link">Как работает Кудалибо</a>
+					</li>
+					<li class="site-menu__item">
+						<a href="#" class="site-menu__item-link">О компании</a>
+					</li>
+					<li class="site-menu__item">
+						<a href="#" class="site-menu__item-link">Отзывы</a>
+					</li>
+					<li class="site-menu__item">
+						<a href="#" class="site-menu__item-link">Частые вопросы</a>
+					</li>
+				</ul>
+				
+				<span class="site-menu__block-title">Пользователям</span>
+				<ul class="site-menu__block-list">
+					<li class="site-menu__item">
+						<a href="#" class="site-menu__item-link">Связаться с нами</a>
+					</li>
+					<li class="site-menu__item">
+						<a href="#" class="site-menu__item-link">Последние новости</a>
+					</li>
+					<li class="site-menu__item">
+						<a href="#" class="site-menu__item-link">Доверие и безопасность</a>
+					</li>
+					<li class="site-menu__item">
+						<a href="#" class="site-menu__item-link">Политика конфиденциальности</a>
+					</li>
+				</ul>
+			</nav>
+			
+			<div class="site-menu__content-block">
+				<span class="site-menu__block-title">Работа в Кудалибо</span>
+				<div class="site-menu__block-content">
+					<p>Кудалибо — это уникальный сервис, где водители сами назначают цену за поездку, а пользователи всегда
+						имеют возможность выбора.</p>
+					<p>Мы также предлагаем выгодные условия для партнёров, которые готовы разместить виджет Кудалибо на
+						своём сайте или выставлять новые заказы.</p>
+				</div>
+				<div class="site-menu__banners">
+					<div class="site-menu__banners-left">
+						<a href="#" class="image-link">
+							<img src="~assets/img/driver-link.jpg" alt="Водителям">
+							<span>Водителям</span>
+						</a>
+						<a href="#" class="image-link">
+							<img src="~assets/img/partner-link.jpg" alt="Партнёрам">
+							<span>Партнёрам</span>
+						</a>
+					</div>
+					<a href="#" class="site-menu__banners-right">
+						<div class="blogs-banner cookie-banner cookie-banner--black">
+							<h3 class="cookie-banner__title">Не знаете, куда поехать в Крыму ?</h3>
+							<p class="cookie-banner__text">Мы составили полный перечень достопримечательностей Крыма на любой вкус и
+								кошелёк!</p>
+							<span class="btn btn--gray">Узнать больше</span>
+							<img class="cookie-banner__image" src="~assets/img/crimea-blogs-logo.png" alt="Логотип Крыма">
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<Social class="site-menu__socials" />
+	</div>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style lang="scss">
+	@import '~assets/scss/variables';
+	@import '~assets/scss/mixins';
+	
+	.site-menu {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: 3;
+		visibility: hidden;
+		padding: 30px 50px;
+		opacity: 0;
+		border-left: 1px solid rgba(255, 255, 255, .5);
+		background: rgba(0, 0, 0, .3);
+		color: $light;
+		
+		&__title {
+			color: $light;
+			font-size: 24px;
+			font-weight: 300;
+			margin-bottom: 95px;
+		}
+		
+		&__wrap {
+			display: flex;
+		}
+		
+		&__block {
+			max-width: 230px;
+			min-width: 230px;
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			margin-right: 60px;
+		}
+		
+		&__block-title {
+			font-size: 20px;
+			font-weight: 700;
+			margin-bottom: 12px;
+			display: block;
+		}
+		
+		&__content-block {
+			max-width: 585px;
+		}
+		
+		&__block-content {
+			max-width: 566px;
+			margin-bottom: 35px;
+			
+			p {
+				margin-bottom: 10px;
+			}
+		}
+		
+		&__block-list {
+			display: flex;
+			flex-direction: column;
+			margin-bottom: 45px;
+		}
+		
+		&__item {
+			margin-bottom: 10px;
+		}
+		
+		&__item-link {
+			color: $light;
+			font-size: 16px;
+			font-weight: 400;
+			@include underline($light);
+			padding-bottom: 1px;
+			line-height: 1.3;
+			
+			&:hover {
+				color: $accent-color;
+				@include underline($accent-color)
+			}
+		}
+		
+		&__banners {
+			display: flex;
+		}
+		
+		&__banners-left {
+			min-width: 300px;
+			margin-right: 18px;
+			
+			.image-link {
+				margin-bottom: 18px;
+			}
+		}
+	}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,30 +3,26 @@
 		<div class="app__sidebar" :class="{'app__sidebar--settings-open': settingsShown}">
 			<Logo descColor="white"/>
 			
-			
+			<!-- Иконка меню -->
 			<Btn class="burger btn btn--dark btn--no-text btn--rounded" :class="{'burger--active': menuOpened}" @click.native="menuOpened = !menuOpened">
 				<div slot="icon">
-					<transition name="scale" mode="out-in">
-						<svg key="1" v-if="!menuOpened" width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<svg width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<rect y="2" width="25" height="2" rx="1" fill="white"/>
 							<rect y="10" width="25" height="2" rx="1" fill="white"/>
 							<rect y="18" width="20" height="2" rx="1" fill="white"/>
 							<circle cx="25" cy="3" r="3" fill="#FF9332"/>
-						</svg>
-						<svg key="2" v-else width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M6.66675 6.66669L16.0001 16M16.0001 16L25.3334 25.3334M16.0001 16L25.3334 6.66669M16.0001 16L6.66675 25.3334" stroke="url(#closeLinear)" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M1.66675 1.66669L11.0001 11M11.0001 11L20.3334 20.3334M11.0001 11L20.3334 1.66669M11.0001 11L1.66675 20.3334" stroke="url(#burger-linear)" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
 							<defs>
-								<linearGradient id="closeLinear" x1="16.0001" y1="6.66669" x2="16.0001" y2="25.3334" gradientUnits="userSpaceOnUse">
+								<linearGradient id="burger-linear" x1="11.0001" y1="1.66669" x2="11.0001" y2="20.3334" gradientUnits="userSpaceOnUse">
 									<stop stop-color="white"/>
 									<stop offset="0.760417" stop-color="#DD8D46"/>
 									<stop offset="1" stop-color="#DD8D46"/>
 								</linearGradient>
 							</defs>
 						</svg>
-					</transition>
+				
 				</div>
 			</Btn>
-			
 			
 			<!--	Дополнительные настройки заказа	-->
 			<transition name="fadeToLeft">
@@ -241,7 +237,7 @@
 				<StaticMenu/>
 				
 				<div class="user-buttons">
-					<Btn class="burger btn btn--dark btn--rounded">
+					<Btn class="btn btn--dark btn--rounded">
 						<svg slot="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M13 8C13.7956 8 14.5587 8.31607 15.1213 8.87868C15.6839 9.44129 16 10.2044 16 11C16 11.2652 16.1054 11.5196 16.2929 11.7071C16.4804 11.8946 16.7348 12 17 12C17.2652 12 17.5196 11.8946 17.7071 11.7071C17.8946 11.5196 18 11.2652 18 11C18 9.67392 17.4732 8.40215 16.5355 7.46447C15.5979 6.52678 14.3261 6 13 6C12.7348 6 12.4804 6.10536 12.2929 6.29289C12.1054 6.48043 12 6.73478 12 7C12 7.26522 12.1054 7.51957 12.2929 7.70711C12.4804 7.89464 12.7348 8 13 8Z" fill="#DD8D46"/>
 							<path d="M13 4C14.8565 4 16.637 4.7375 17.9497 6.05025C19.2625 7.36301 20 9.14348 20 11C20 11.2652 20.1054 11.5196 20.2929 11.7071C20.4804 11.8946 20.7348 12 21 12C21.2652 12 21.5196 11.8946 21.7071 11.7071C21.8946 11.5196 22 11.2652 22 11C22 8.61305 21.0518 6.32387 19.364 4.63604C17.6761 2.94821 15.3869 2 13 2C12.7348 2 12.4804 2.10536 12.2929 2.29289C12.1054 2.48043 12 2.73478 12 3C12 3.26522 12.1054 3.51957 12.2929 3.70711C12.4804 3.89464 12.7348 4 13 4ZM21.75 15.91C21.695 15.7495 21.6 15.6056 21.4741 15.4919C21.3481 15.3781 21.1953 15.2984 21.03 15.26L15.03 13.89C14.8671 13.8531 14.6976 13.8575 14.5369 13.9029C14.3762 13.9484 14.2294 14.0333 14.11 14.15C13.97 14.28 13.96 14.29 13.31 15.53C11.1532 14.5362 9.42499 12.8009 8.44 10.64C9.71 10 9.72 10 9.85 9.85C9.96671 9.73056 10.0516 9.58379 10.0971 9.42308C10.1425 9.26238 10.1469 9.09286 10.11 8.93L8.74 3C8.70165 2.8347 8.62186 2.68187 8.50814 2.55591C8.39443 2.42995 8.25053 2.335 8.09 2.28C7.85647 2.19659 7.61531 2.1363 7.37 2.1C7.11725 2.0414 6.85933 2.0079 6.6 2C5.38 2 4.20998 2.48464 3.34731 3.34731C2.48464 4.20998 2 5.38 2 6.6C2.00529 10.6827 3.62949 14.5967 6.5164 17.4836C9.40331 20.3705 13.3173 21.9947 17.4 22C18.0041 22 18.6022 21.881 19.1603 21.6498C19.7184 21.4187 20.2255 21.0798 20.6527 20.6527C21.0798 20.2255 21.4187 19.7184 21.6498 19.1603C21.881 18.6022 22 18.0041 22 17.4C22.0003 17.1455 21.9802 16.8913 21.94 16.64C21.898 16.3916 21.8344 16.1474 21.75 15.91ZM17.4 20C13.8469 19.9974 10.4401 18.5847 7.92769 16.0723C5.41528 13.5599 4.00265 10.1531 4 6.6C4.00263 5.91125 4.27741 5.25146 4.76443 4.76443C5.25146 4.27741 5.91125 4.00263 6.6 4H6.93L8 8.64L7.46 8.92C6.6 9.37 5.92 9.73 6.28 10.51C6.86622 12.1696 7.81477 13.6777 9.05674 14.9249C10.2987 16.172 11.8029 17.1269 13.46 17.72C14.3 18.06 14.63 17.43 15.08 16.56L15.37 16.01L20 17.07V17.4C19.9974 18.0888 19.7226 18.7485 19.2356 19.2356C18.7485 19.7226 18.0888 19.9974 17.4 20Z" fill="white"/>
@@ -251,7 +247,7 @@
 					</Btn>
 					
 					<div class="user-buttons__dropdown">
-						<Btn class="burger btn btn--dark btn--rounded">
+						<Btn class="btn btn--dark btn--rounded">
 							<svg slot="icon" width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<ellipse cx="12.1202" cy="6.5" rx="4" ry="4.5" stroke="white" stroke-width="1.8"/>
 								<path d="M20.1202 19C20.1202 22.5 16.1202 21.5 12.1202 21.5C8.12024 21.5 4.12024 22.5 4.12024 19C4.12024 17 7.70196 14.5 12.1202 14.5C16.5385 14.5 20.1202 17 20.1202 19Z" stroke="#DD8D46" stroke-width="1.8"/>
@@ -275,61 +271,7 @@
 			<div class="app__banner"></div>
 			
 			<transition name="fadeToRight">
-				<div class="site-menu">
-					<strong class="site-menu__title">Меню сайта</strong>
-					<div class="site-menu__lists">
-						<nav class="site-menu__block">
-							<span class="site-menu__block-title">Информация</span>
-							<ul class="site-menu__block-list">
-								<li class="site-menu__item">
-									<a href="#" class="site-menu__item-link">Как работает Кудалибо</a>
-									<a href="#" class="site-menu__item-link">О компании</a>
-									<a href="#" class="site-menu__item-link">Отзывы</a>
-									<a href="#" class="site-menu__item-link">Частые вопросы</a>
-								</li>
-							</ul>
-							
-							<span class="site-menu__block-title">Пользователям</span>
-							<ul class="site-menu__block-list">
-								<li class="site-menu__item">
-									<a href="#" class="site-menu__item-link">Связаться с нами</a>
-									<a href="#" class="site-menu__item-link">Последние новости</a>
-									<a href="#" class="site-menu__item-link">Доверие и безопасность</a>
-									<a href="#" class="site-menu__item-link">Политика конфиденциальности</a>
-								</li>
-							</ul>
-						</nav>
-						
-						<div class="site-menu__block">
-							<span class="site-menu__block-title">Работа в Кудалибо</span>
-							<div class="site-menu__block-content">
-								<p>Кудалибо — это уникальный сервис, где водители сами назначают цену за поездку, а пользователи всегда
-									имеют возможность выбора.</p>
-								<p>Мы также предлагаем выгодные условия для партнёров, которые готовы разместить виджет Кудалибо на
-									своём сайте или выставлять новые заказы.</p>
-							</div>
-							<div class="site-menu__banners">
-								<div class="site-menu__banners-left">
-									<a href="#" class="image-link">
-										<img src="~assets/img/driver-link.jpg" alt="Водителям">
-										<span>Водителям</span>
-									</a>
-									<a href="#" class="image-link">
-										<img src="~assets/img/partner-link.jpg" alt="Партнёрам">
-										<span>Партнёрам</span>
-									</a>
-								</div>
-								<div class="site-menu__banners-right">
-									<div class="blogs-banner cookie-banner cookie-banner--black">
-										<h3 class="cookie-banner__title">Не знаете, куда поехать в Крыму ?</h3>
-										<p class="cookie-banner__te"></p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				
-				</div>
+				<SiteMenu/>
 			</transition>
 			
 			<div class="app__back" :class="{'app__back--active': modalBackShown}"></div>
@@ -338,16 +280,11 @@
 </template>
 
 <script>
-	import Logo from '~/components/Logo.vue'
-	import Btn from '~/components/Btn.vue'
-	import StaticMenu from '~/components/StaticMenu.vue'
-	import Copyright from '~/components/Copyright.vue'
 	// import {ValidationProvider, ValidationObserver} from "vee-validate";
 
 	export default {
 		layout: ({isMobile}) => isMobile ? 'app-mobile' : 'app',
 		components: {
-			Copyright
 			/*ValidationProvider,
 			ValidationObserver,*/
 		},
